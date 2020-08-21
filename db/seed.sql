37257ea17b9b83aad268978149f71bfee90f9d04
Tripti Shres <tripti.shres@gmail.com>
2020-08-21 21:24:20 +1000

    Inital commit

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUEs ("Spicy Grilled Chicke", false),
+("Grilled Chicken", false),
+("Big Mac", false),
+("Veggie Burger", true),
+("Whooper", true),
+("Spicy Mc Chicken",true);
